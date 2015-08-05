d530f936e002419b0cada1e25348df746daa3043
uwyang <uwyang@gmail.com>
2015-08-05 02:01:30 -0400

    commit the new  urbispot project after the last one is corrupted due to missing csproject file

---
+++ Projects/ParkShare/urbispot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(urbispot.Startup))]
+namespace urbispot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
